77bb06d1d6081b6d7bca3ebff0cea8d30c67a42d
sumonwin <sumonwin@cloudsource-VirtualBox>
2014-02-03 16:33:38 +0630

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Attendanceproject::Application.config.secret_token = 'c3ee8775e70a61be951c9c07b00f81898c7992f2e660545bef8729dfb2c349c130b4de9abb546a3ce92dbc7974392cf578e9a57ad88d25dd9afee4fb37a72362'
